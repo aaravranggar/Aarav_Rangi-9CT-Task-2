41252db9dea5c4154e082d2f8372d1ac2cf087ef
aaravranggar <aarav.rangi@education.nsw.gov.au>
2026-08-07 12:26:28 +1000

    Update main.py

--- main.py
+++ main.py
@@ -3,33 +3,34 @@ import onewire
 import ds18x20
 import time
 
-# Temperature limit
-LIMIT = 40
+def OverheatPreset():   # preset 1
+    limit = 40
 
-# Sensor placement
-ds = machine.Pin(26)
-ow = onewire.OneWire(ds)
-sensor = ds18x20.DS18X20(ow)
+    # sensor setup
+    ds = machine.Pin(26)
+    ow = onewire.OneWire(ds)
+    sensor = ds18x20.DS18X20(ow)
 
-# LED and buzzer
-led = machine.Pin(15, machine.Pin.OUT)
-buzzer = machine.Pin(14, machine.Pin.OUT)
+    # LED + buzzer
+    led = machine.Pin(15, machine.Pin.OUT)
+    buzzer = machine.Pin(14, machine.Pin.OUT)
 
-# Find sensor
-roms = sensor.scan()
-print(roms)
+    # find sensor
+    roms = sensor.scan()
+    print("Sensor:", roms)
 
-if len(roms) == 0:
-    print("No sensor found")
-else:
+    if len(roms) == 0:
+        print("No sensor found")
+        return
+
     while True:
         sensor.convert_temp()
         time.sleep_ms(750)
-
         temp = sensor.read_temp(roms[0])
+
         print("Temp:", temp)
 
-        if temp > LIMIT:
+        if temp > limit:
             led.value(1)
             buzzer.value(1)
         else:
@@ -37,3 +38,4 @@ else:
             buzzer.value(0)
 
         time.sleep(5)
+ 
